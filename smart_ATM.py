44e84766454aa03e4f4120220fd350a4b16d2fd4
dragon11175 <lalitsainiak23@gmail.com>
2025-04-11 06:50:53 +0000

    ADD OPTIONS FOR USER CHOICE

--- smart_ATM.py
+++ smart_ATM.py
@@ -1,4 +1,17 @@
 def smartATM():
-    print("WELCOME TO SMART_ATM")
+    currect_pin=8059
+    print("WELCOME TO SMART_ATM\n")
+    print("ENTER YOUR PIN CODE: ")
+    pin=int(input("ENTER YOUR PIN CODE"))
+
+    if pin==currect_pin:
+        print("1.check balance:\n2.deposite\n3.withdraw\n4.exit\n")
+        choice=int(input("ENTER YOUR OPTION:"))
+
+    else:
+        print("INVALID PIN!")
+
+
+    
 
 smartATM()    
