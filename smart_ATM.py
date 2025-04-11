13bafab5f647429073de269f577db0b474af6710
dragon11175 <lalitsainiak23@gmail.com>
2025-04-11 08:51:33 +0000

    UPGRADE DEPOSIT AND WITHDRAW

--- smart_ATM.py
+++ smart_ATM.py
@@ -1,12 +1,25 @@
 def smartATM():
     currect_pin=8059
-    print("WELCOME TO SMART_ATM\n")
-    print("ENTER YOUR PIN CODE: ")
-    pin=int(input("ENTER YOUR PIN CODE"))
+    current_balance=5000
+    print("\n\n!WELCOME TO SMART_ATM!\n")
+    pin=int(input("ENTER YOUR PIN CODE:"))
 
     if pin==currect_pin:
         print("1.check balance:\n2.deposite\n3.withdraw\n4.exit\n")
         choice=int(input("ENTER YOUR OPTION:"))
+        if choice==1:
+            print("YOUR CURRENT BALANCE NOW:",current_balance)
+
+        elif choice==2:
+            deposite=int(input("ENTER HOW MUCH MONEY YOU WANT DEPOSITE:"))
+            if deposite<=0:
+                print("INVALID AMMOUNT")
+            else:
+                current_balance=current_balance+deposite
+                print(f"SUCCESFULL!\n\nyour current balance is now:{current_balance}")
+
+
+
 
     else:
         print("INVALID PIN!")
