6a23aca2378bf0fcf50e68f1d31d6c8d40d9e663
dragon11175 <lalitsainiak23@gmail.com>
2025-04-12 05:40:57 +0000

    80 percent atm complete testing is remaning

--- smart_ATM.py
+++ smart_ATM.py
@@ -18,13 +18,19 @@ def smartATM():
                 current_balance=current_balance+deposite
                 print(f"SUCCESFULL!\n\nyour current balance is now:{current_balance}")
 
-
-
+        elif choice==3:
+            withdraw=int(input("ENTER YOUR WITHDRAW AMMOUNT:"))
+            if withdraw<=0 and withdraw>current_balance:
+                print("PLEASE ENTER VALID AMMOUNT")
+            else:
+                current_balance=current_balance-withdraw
+                print("YOUR WITHDRAW SUCCESSFULL!",current_balance)
+        
+        elif choice==4:
+            print("GOOD BYE !\nHAVE YOUE A GOOD DAY!")
 
     else:
         print("INVALID PIN!")
 
-
-    
 
 smartATM()    
